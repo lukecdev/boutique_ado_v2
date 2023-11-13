acbb4a7a61a416fb97bea2a5d66a30a2728e19ad
lukecdev <courtney.luke@outlook.com>
2023-11-13 21:49:56 +0000

    started product template

--- products/views.py
+++ products/views.py
@@ -4,12 +4,12 @@ from .models import Product
 # Create your views here.
 
 def all_products(request):
-    """ A view to show all products, inculding sorting and search queries """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
 
-    contect = {
+    context = {
         'products': products,
     }
-    
+
     return render(request, 'products/products.html', context)
