cc613dd1f4483ef100fea061eb30a49f00d639dc
lukecdev <courtney.luke@outlook.com>
2023-11-21 19:32:00 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form' : order_form,
+        'stripe_public_key': 'pk_test_51OEyoBAcR8F7xekpI7MqgantlNVVR6V1u1vnPiNudxFYiBQoVDhvqFAZ1vKNE4bQs1zT7Sf8uGzgOkFYPd0yanzO00VsIxW7d3',
+        'client_secret': 'test_client_secret',
     }
 
     return render(request, template, context)
