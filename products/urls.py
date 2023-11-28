dbe879c7f9c4441b0cbd0e0f6e8a7f7fac62b714
lukecdev <courtney.luke@outlook.com>
2023-11-28 17:30:36 +0000

    editing products

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_products, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
